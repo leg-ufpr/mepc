9259a2194493601135c9b9df7b8115378c93c978
Fernando Mayer <fernandomayer@gmail.com>
2021-03-16 19:47:45 -0300

    Inclui arquivos de rmarkdown

---
+++ rmarkdown/00_LEIAME.R
@@ -0,0 +1,13 @@
+## Antes de rodar os arquivos Rmd (R Markdown), instale os seguintes pacotes
+## (simplesmente rode esse comando):
+install.packages(c("knitr", "rmarkdown", "xtable", "bookdown", "rticles"))
+
+## Abra qualquer arquivo Rmd e clique no notão 'Knit'
+
+## Você pode criar um novo arquivo Rmd no menu
+## 'File > New file > R Markdown...'
+## Na janela que abrir selecione as informações e clique em 'Ok'
+## Nessa janela, você pode também clicar em 'From template' e selecionar
+## o template de alguma revista científica, já disponibilizado nesse
+## formato (esses templates são do pacote 'rticles' instalado acima).
+
